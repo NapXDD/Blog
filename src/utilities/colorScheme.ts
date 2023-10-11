51b2f596c2a77dd1c291358a133562f71b022ac9
Đỗ Lê Anh Khoa <khoadole21@gmail.com>
2023-10-11 11:24:34 +0700

    lmao blog

---
+++ src/utilities/colorScheme.ts
@@ -0,0 +1,10 @@
+const colorScheme = {
+  pink1: "#e39eb6",
+  pink2: "#edb4be",
+  pink3: "#f5cbcb",
+  blue1: "#425e80",
+  blue2: "#6ca1b6",
+  blue3: "#86b0be",
+};
+
+export default colorScheme;
